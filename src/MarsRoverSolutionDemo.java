e4916cc7c6ea9995e53f7fea68ebed1f8a58d011
thusitha <thusitha.dissanayaka@rezgateway.com>
2020-05-04 14:03:16 +0530

    added

--- src/MarsRoverSolutionDemo.java
+++ src/MarsRoverSolutionDemo.java
@@ -2,17 +2,25 @@
 public class MarsRoverSolutionDemo {
 
 	public static void main(String[] args) {
+		try {
+			// Expected Output: 1 3 N
+			MarsRover rover = new MarsRover();
+			rover.setPosition(1, 2, "N");
+			rover.process("LMLMLMLMM");
+			rover.printRoverPosition();
 
-		// Expected Output: 1 3 N
-		MarsRover rover = new MarsRover();
-		rover.setPosition(1, 2, "N");
-		rover.process("LMLMLMLMM");
-		rover.printRoverPosition();
+			/**Next command should run after first command is completed, I assumed it will take 1000 Ms time */
+			Thread.sleep(1000);
+			
+			// Expected Output: 5 1 E
+			rover = new MarsRover();
+			rover.setPosition(3, 3, "E");
+			rover.process("MMRMMRMRRM");
+			rover.printRoverPosition();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		// Expected Output: 5 1 E
-		rover.setPosition(3, 3, "E");
-		rover.process("MMRMMRMRRM");
-		rover.printRoverPosition();
 	}
 
 }
